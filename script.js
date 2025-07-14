e806e02959eedf7021e2a3073a158f672dfce69c
barbi2025 <barbiapuntes@gmail.com>
2025-07-14 03:17:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Definir todos los ramos por semestre, con sus requisitos y desbloqueos
   const malla = {
-    "I semestre": [
+    "1° semestre": [
       { nombre: "Introducción a la profesión", desbloquea: [] },
       { nombre: "Química general y orgánica", desbloquea: ["Bioquímica clínica"] },
       { nombre: "Biología molecular de la célula", desbloquea: ["Bioquímica clínica", "Desarrollo embrio-fetal"] },
       { nombre: "Anatomía general", desbloquea: ["Morfología reproductiva", "Fisiología"] },
       { nombre: "Taller de desarrollo personal", desbloquea: [] }
     ],
-    "II semestre": [
+    "2° semestre": [
       { nombre: "Salud pública", desbloquea: ["Salud familiar y comunitaria", "Promoción y educación para la salud II", "Epidemiología clínica y social"] },
       { nombre: "Promoción y educación para la salud I", desbloquea: ["Promoción y educación para la salud II"] },
       { nombre: "Bioética y aspectos legales", desbloquea: [] },
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
       { nombre: "Psicología del desarrollo", desbloquea: ["Psicología de la mujer"] },
       { nombre: "Inglés I", desbloquea: ["Inglés II"] }
     ],
-    "III semestre": [
+    "3° semestre": [
       { nombre: "Salud familiar y comunitaria", requisitos: ["Salud pública"], desbloquea: ["Obstetricia I", "Ginecología I", "Neonatología I"] },
       { nombre: "Promoción y educación para la salud II", requisitos: ["Promoción y educación para la salud I", "Salud pública"], desbloquea: ["Clínica proceso de enfermería", "Atención primaria obstétrica"] },
       { nombre: "Epidemiología clínica y social", requisitos: ["Salud pública"], desbloquea: [] },
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
       { nombre: "Microbiología y parasitología clínica", requisitos: ["Bioquímica clínica"], desbloquea: ["Proceso de atención de enfermería"] },
       { nombre: "Bioestadística", desbloquea: ["Investigación en salud I"] }
     ],
-    "IV semestre": [
+    "4° semestre": [
       { nombre: "Obstetricia I", requisitos: ["Salud familiar y comunitaria", "Desarrollo embrio-fetal"], desbloquea: ["Obstetricia II", "Atención primaria obstétrica"] },
       { nombre: "Ginecología I", requisitos: ["Salud familiar y comunitaria", "Desarrollo embrio-fetal"], desbloquea: ["Ginecología II"] },
       { nombre: "Neonatología I", requisitos: ["Salud familiar y comunitaria", "Desarrollo embrio-fetal"], desbloquea: ["Neonatología II"] },
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", () => {
       { nombre: "Fisiopatología", requisitos: ["Fisiología"], desbloquea: ["Obstetricia II", "Ginecología II", "Neonatología II"] },
       { nombre: "Inglés II", requisitos: ["Inglés I"], desbloquea: ["Inglés III"] }
     ],
-    "V semestre": [
+    "5° semestre": [
       { nombre: "Obstetricia II", requisitos: ["Obstetricia I", "Fisiopatología"], desbloquea: ["Integración profesional"] },
       { nombre: "Ginecología II", requisitos: ["Ginecología I", "Fisiopatología"], desbloquea: ["Sexualidad y sexología", "Integración profesional", "Atención primaria ginecológica"] },
       { nombre: "Neonatología II", requisitos: ["Neonatología I", "Fisiopatología"], desbloquea: ["Integración profesional", "Clínica en Neonatología I"] },
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
       { nombre: "Clínica proceso de enfermería", requisitos: ["Proceso de atención de enfermería", "Promoción y educación para la salud II"], desbloquea: ["Enfermería médico-quirúrgica"] },
       { nombre: "Atención primaria obstétrica", requisitos: ["Promoción y educación para la salud II", "Obstetricia I"], desbloquea: ["Atención primaria ginecológica"] }
     ],
-    "VI semestre": [
+    "6° semestre": [
       { nombre: "Investigación en salud II", requisitos: ["Investigación en salud I"], desbloquea: ["Proyecto de licenciatura I"] },
       { nombre: "Sexualidad y sexología", requisitos: ["Ginecología II"], desbloquea: [] },
       { nombre: "Integración profesional", requisitos: ["Obstetricia II", "Ginecología II", "Neonatología II"], desbloquea: ["Clínica de servicios gineco-obstétricos I", "Clínica en Neonatología I", "Proceso de intervención comunitaria"] },
@@ -52,7 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
       { nombre: "Atención primaria ginecológica", requisitos: ["Atención primaria obstétrica", "Ginecología II"], desbloquea: ["Gestión en salud I", "Clínica de servicios gineco-obstétricos I"] },
       { nombre: "Inglés III", requisitos: ["Inglés II"], desbloquea: [] }
     ],
-    "VII semestre": [
+    "7° semestre": [
       { nombre: "Proyecto de licenciatura I", requisitos: ["Investigación en salud II"], desbloquea: ["Proyecto de licenciatura II"] },
       { nombre: "Gestión en salud I", requisitos: ["Atención primaria ginecológica"], desbloquea: ["Gestión en salud II"] },
       { nombre: "Clínica de servicios gineco-obstétricos I", requisitos: ["Integración profesional", "Atención primaria ginecológica"], desbloquea: ["Clínica de servicios gineco-obstétricos II"] },
@@ -60,20 +60,20 @@ document.addEventListener("DOMContentLoaded", () => {
       { nombre: "Proceso de intervención comunitaria", requisitos: ["Integración profesional"], desbloquea: ["Internado de atención primaria y comunitaria"] },
       { nombre: "Optativo de formación general", desbloquea: ["Internado integral"] }
     ],
-    "VIII semestre": [
+    "8° semestre": [
       { nombre: "Proyecto de licenciatura II", requisitos: ["Proyecto de licenciatura I"] },
       { nombre: "Gestión en salud II", requisitos: ["Gestión en salud I"], desbloquea: ["Gestión en salud III"] },
       { nombre: "Clínica de servicios gineco-obstétricos II", requisitos: ["Clínica de servicios gineco-obstétricos I"], desbloquea: ["Internado intrahospitalario"] },
       { nombre: "Clínica en Neonatología II", requisitos: ["Clínica en Neonatología I"], desbloquea: ["Internado intrahospitalario"] },
       { nombre: "Electivo de formación I", desbloquea: ["Internado integral"] }
     ],
-    "IX semestre": [
+    "9° semestre": [
       { nombre: "Gestión en salud III", requisitos: ["Gestión en salud II"], desbloquea: ["Internado integral"] },
       { nombre: "Internado intrahospitalario", requisitos: ["Clínica de servicios gineco-obstétricos II", "Clínica en Neonatología II"], desbloquea: ["Internado integral"] },
       { nombre: "Internado de atención primaria y comunitaria", requisitos: ["Proceso de intervención comunitaria"], desbloquea: ["Internado integral"] },
       { nombre: "Electivo de formación II", desbloquea: ["Internado integral"] }
     ],
-    "X semestre": [
+    "10° semestre": [
       { nombre: "Internado integral", requisitos: ["Gestión en salud III", "Internado intrahospitalario", "Internado de atención primaria y comunitaria", "Electivo de formación I", "Electivo de formación II"] },
       { nombre: "Trabajo de titulación" },
       { nombre: "Examen de titulación" }
